8798baed304aae7ea0063dc7339cb7bba31a1ebe
Milan Aleksić <milanaleksic@gmail.com>
2022-05-14 11:16:28 +0200

    add support for the latest oreilly CSV import

--- oreilly/content-extractor.go
+++ oreilly/content-extractor.go
@@ -12,18 +12,38 @@ import (
 	"time"
 )
 
+type format byte
+
+const (
+	formatUnknown format = iota
+	formatV1
+	formatV2
+)
+
 var (
-	isbnRegex          = regexp.MustCompile(`(?:97[89])?\d{9}(?:\d|X)`)
-	supportedCsvFormat = []string{
-		"Book Title",
-		"Authors",
-		"Chapter Title",
-		"Date of Highlight",
-		"Book URL",
-		"Chapter URL",
-		"Highlight URL",
-		"Highlight",
-		"Personal Note",
+	isbnRegex = regexp.MustCompile(`(?:97[89])?\d{9}(?:\d|X)`)
+	formats   = map[format][]string{
+		formatV1: {
+			"Book Title",
+			"Authors",
+			"Chapter Title",
+			"Date of Highlight",
+			"Book URL",
+			"Chapter URL",
+			"Highlight URL",
+			"Highlight",
+			"Personal Note",
+		},
+		formatV2: {
+			"Book Title",
+			"Chapter Title",
+			"Date of Highlight",
+			"Book URL",
+			"Chapter URL",
+			"Annotation URL",
+			"Highlight",
+			"Personal Note",
+		},
 	}
 )
 
@@ -45,6 +65,7 @@ func (e *ContentExtractor) IngestRecords(ctx context.Context, reader io.Reader) 
 	begin := time.Now()
 	r := csv.NewReader(reader)
 	firstRecord := true
+	version := formatUnknown
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -54,12 +75,21 @@ func (e *ContentExtractor) IngestRecords(ctx context.Context, reader io.Reader) 
 			return err
 		}
 		if firstRecord {
-			if !reflect.DeepEqual(record, supportedCsvFormat) {
-				return fmt.Errorf("CSV does not have expected format: %+v encountered, but expected: %v", record, supportedCsvFormat)
+			if reflect.DeepEqual(record, formats[formatV1]) {
+				version = formatV1
+			} else if reflect.DeepEqual(record, formats[formatV2]) {
+				version = formatV2
+			} else {
+				return fmt.Errorf("CSV does not have expected format: %+v encountered, but expected one of: %+v", record, formats)
 			}
+			log.Infof("Proceeding with format version %v", version)
 			firstRecord = false
 		} else {
-			err := e.ingestRecord(ctx, record)
+			if version == formatV1 {
+				err = e.ingestRecordV1(ctx, record)
+			} else if version == formatV2 {
+				err = e.ingestRecordV2(ctx, record)
+			}
 			if err != nil {
 				return fmt.Errorf("error while ingesting row %+v: %w", record, err)
 			}
@@ -70,7 +100,7 @@ func (e *ContentExtractor) IngestRecords(ctx context.Context, reader io.Reader) 
 	return err
 }
 
-func (e *ContentExtractor) ingestRecord(ctx context.Context, record []string) (err error) {
+func (e *ContentExtractor) ingestRecordV1(ctx context.Context, record []string) (err error) {
 	book := &model.Book{
 		Name:    record[0],
 		Authors: record[1],
@@ -99,6 +129,51 @@ func (e *ContentExtractor) ingestRecord(ctx context.Context, record []string) (e
 		Location: model.Location{},
 		Ts:       parsedTime,
 		Origin:   record[4],
+		Type:     model.Highlight,
+	}
+	existed, err := e.annotationRepo.UpsertAnnotation(ctx, a)
+	if err != nil {
+		log.Errorf("Failed to upsert an annotation: %v", err)
+		return
+	}
+	if existed {
+		e.annotationsUpdated++
+	} else {
+		e.annotationsInserted++
+	}
+
+	return
+}
+
+func (e *ContentExtractor) ingestRecordV2(ctx context.Context, record []string) (err error) {
+	book := &model.Book{
+		Name:    record[0],
+		Authors: "",
+		Isbn:    "",
+	}
+
+	submatch := isbnRegex.FindAllStringSubmatch(record[3], -1)
+	if len(submatch) == 0 {
+		return fmt.Errorf("could not match the ISBN in the book page URL: %v", record[3])
+	}
+	book.Isbn = submatch[0][0]
+
+	_, err = e.bookRepo.UpsertBook(ctx, book)
+	if err != nil {
+		log.Errorf("Failed to upsert a book: %v", err)
+		return
+	}
+
+	parsedTime, err := time.Parse("2006-01-02", record[2])
+	if err != nil {
+		return fmt.Errorf("could not parsedTime the day of highlight from %v: %w", record[2], err)
+	}
+	a := &model.Annotation{
+		BookId:   book.Id,
+		Text:     record[6],
+		Location: model.Location{},
+		Ts:       parsedTime,
+		Origin:   record[3],
 		Type:     model.Highlight,
 	}
 	existed, err := e.annotationRepo.UpsertAnnotation(ctx, a)
